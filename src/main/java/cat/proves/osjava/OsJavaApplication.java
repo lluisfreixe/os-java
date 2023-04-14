73f2a1d3b9493ae59c2917b86ad18f50d35be507
IT01169 <IT01169@LAPT0805.DGP.INTERIOR.INTRANET>
2023-04-14 10:41:50 +0200

    initial commit

---
+++ src/main/java/cat/proves/osjava/OsJavaApplication.java
@@ -0,0 +1,13 @@
+package cat.proves.osjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/proves/osjava/OsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.proves.osjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
